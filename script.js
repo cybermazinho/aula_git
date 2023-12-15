f487b929e4a2ea5abd3dd4c772f2c14d7490538c
cybermazinho <bryant0rr3sribeiro@gmail.com>
2023-12-14 21:25:19 -0500

    alguma coisa

---
+++ script.js
@@ -0,0 +1,7 @@
+//git clone = serva para baixa um repositorio github
+
+//git add . = empacotando codigo
+//git commit -m "alguma coisa" = colocando etiqueda no codigo
+//git push = subindo codigo "mandando pro despache"
+
+console.log("hello world")
